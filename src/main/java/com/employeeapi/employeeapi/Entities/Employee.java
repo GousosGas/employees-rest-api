5191a947b0a18c3fe9e94b7144776b230c105d3d
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-08-26 17:43:46 +0300

    Exposed id

--- src/main/java/com/employeeapi/employeeapi/Entities/Employee.java
+++ src/main/java/com/employeeapi/employeeapi/Entities/Employee.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Employee {
 
 @Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 @Column(name="id")
 private Long id;
 @Column(name = "first_name")
@@ -20,4 +20,7 @@ private String firstName;
 @Column(name = "last_name")
 private String lastName;
 
+public Long getResourceId(){
+    return id;
+}
 }
